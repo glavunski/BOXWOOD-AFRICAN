ee4a0e71dab9db5584a45f6212e743b5115a52eb
glavunski <xiii1994@abv.bg>
2014-07-24 04:23:45 +0300

    opraven movement

--- GAME_PROJECT/eventhandler.js
+++ GAME_PROJECT/eventhandler.js
@@ -1,46 +1,76 @@
 var playerX=parseInt(document.getElementById('player').style.left);
-var tickRate = 30,
+var playerY=parseInt(document.getElementById('player').style.top);
+var tickRate = 15,
     keyArrowLeft  = false,
-    keyArrowRight = false;
+    keyArrowRight = false,
+    keyArrowUp = false,
+    keyArrowDown = false;
 
-function movement(){
+function keyDown(event) {
 
-    document.addEventListener('keydown', function(event) {
 
-        switch(event.keyCode){
-            case 37:
-                keyArrowLeft = true; break;
-            case 39:
-                keyArrowRight= true;  break;
-        }
-    }, false);
-
-    document.addEventListener('keyup', function(event) {
-
-        switch(event.keyCode){
-            case 37: keyArrowLeft = false; break;
-            case 39:  keyArrowRight= false; break;
-        }
-
-    }, false);
-
-    var tick = function() {
-        var limit = parseInt(document.getElementById('gameholder').style.marginLeft);
-        if (keyArrowLeft) {
-            if (playerX > limit) {
-                playerX = playerX - 5;
-            }
-        } else if (keyArrowRight) {
-            if (playerX < limit+800) {
-                playerX = playerX +5;
-            }
-        }
-
-        document.getElementById('player').style.left=playerX.toString() + "px";
-        setTimeout(tick, tickRate);
-    };
-
-    tick();
+    if (event.keyCode === 39) {
+        keyArrowRight = true;
+    } else if (event.keyCode === 37) {
+        keyArrowLeft = true;
+    }
+    if (event.keyCode === 38) {
+        keyArrowUp = true;
+    } else if (event.keyCode === 40) {
+        keyArrowDown= true;
+    }
 
 }
-movement();
+
+function keyUp(event){
+
+    if (event.keyCode === 39) {
+        keyArrowRight = false;
+    } else if (event.keyCode === 37) {
+        keyArrowLeft = false;
+    }
+    if (event.keyCode === 38) {
+        keyArrowUp = false;
+    } else if (event.keyCode === 40) {
+        keyArrowDown = false;
+    }
+
+}
+
+var tick = function() {
+
+    if (keyArrowLeft) {
+        if (playerX > 150) {
+            playerX = playerX - 5;
+        }
+    } else if (keyArrowRight) {
+        if (playerX < 810) {
+            playerX = playerX + 5;
+        }
+    }
+     if(keyArrowUp){
+        if (playerY > 25) {
+            playerY = playerY -5;
+        }
+    }else if(keyArrowDown){
+        if (playerY < 380) {
+            playerY = playerY +5;
+        }
+    }
+
+    document.getElementById('player').style.left=playerX.toString() + "px";
+    document.getElementById('player').style.top=playerY.toString() + "px";
+
+};
+
+window.onload =function movement(){
+    document.addEventListener('keydown', keyDown, false);
+    document.addEventListener('keyup', keyUp, false);
+    function loop() {
+        tick();
+        setTimeout(loop, tickRate);
+
+    }
+
+    loop();
+};
