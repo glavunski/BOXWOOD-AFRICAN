d612706a1c77d8b447a21a426944d16baf78d8d7
glavunski <xiii1994@abv.bg>
2014-07-30 00:41:52 +0300

    Signed-off-by: glavunski <xiii1994@abv.bg>

---
+++ version3/eventhandler.js
@@ -0,0 +1,286 @@
+
+var playGround = document.getElementById('gameholder');
+var playGroundLeft = parseInt(playGround.offsetLeft);
+var playGroundTop = parseInt(playGround.offsetTop);
+var playGroundWidth = parseInt(playGround.clientWidth);
+var playGroundHeight = parseInt(playGround.clientHeight);
+
+
+var player = document.getElementById('player');
+var playerWidth = player.clientWidth;
+var playerHeight = player.clientHeight;
+player.style.left = (playGroundLeft - playerWidth / 2 + playGroundWidth / 2) + 'px';
+player.style.top = (playGroundTop - playerHeight / 2 + playGroundHeight / 2) + 'px';
+var playerX = parseInt(player.style.left);
+var playerY = parseInt(player.style.top);
+var lives = 999;
+
+var las=false;
+
+
+var init = document.getElementById('player').style.webkitTransform.indexOf('(');
+var fin = document.getElementById('player').style.webkitTransform.indexOf(')');
+
+var playerR=parseInt(document.getElementById('player').style.webkitTransform.substr(init+1,fin-init-1));
+var tickRate = 15,
+    keyArrowLeft  = false,
+    keyArrowRight = false,
+    keyArrowUp = false,
+    keyArrowDown = false;
+
+var offsets1;
+var offsets2;
+function keyDown(event) {
+
+
+    if (event.keyCode === 39) {
+        keyArrowRight = true;
+    } else if (event.keyCode === 37) {
+        keyArrowLeft = true;
+    }
+    if (event.keyCode === 38) {
+        keyArrowUp = true;
+    } else if (event.keyCode === 40) {
+        keyArrowDown= true;
+    }
+
+}
+
+function keyUp(event){
+
+    if (event.keyCode === 39) {
+        keyArrowRight = false;
+    } else if (event.keyCode === 37) {
+        keyArrowLeft = false;
+    }
+    if (event.keyCode === 38) {
+        keyArrowUp = false;
+    } else if (event.keyCode === 40) {
+        keyArrowDown = false;
+    }
+
+}
+
+function createLasers(event){
+
+
+     offsets1 = document.getElementById("eye1").getBoundingClientRect();
+        offsets2 = document.getElementById("eye12").getBoundingClientRect();
+
+
+        var lazor = document.createElement("div");
+        var lazor2 = document.createElement("div");
+
+        var px = event.pageX;
+        var py = event.pageY;
+
+
+        lazor.className="lazers";
+        lazor2.className="lazers";
+
+
+        document.body.appendChild(lazor);
+        document.body.appendChild(lazor2);
+
+        var left = (offsets1.left - px);
+        var top = (offsets1.top - py);
+        var rad1 = Math.atan2(top, left);
+
+        var left2 = (offsets2.left - px);
+        var top2 = (offsets2.top - py);
+     var rad2 = Math.atan2(top2, left2);
+
+
+
+    lazor.style.position="absolute";
+    lazor.style.height=5 + "px";
+    lazor.style.width =5 + "px";
+    lazor.style.borderRadius=30 + "px";
+    lazor.style.zIndex = "5";
+    lazor.style.boxShadow ="inset 0 10px 1px white, 0 0 7px red";
+    lazor.style.backgroundColor = "red";
+    lazor.style.TransformOrigin = "50% 100%";
+    lazor.style.transform = "rotate(" + rad1 + "rad)";
+
+    lazor2.style.position="absolute";
+    lazor2.style.height=5 + "px";
+    lazor2.style.width =5 + "px";
+    lazor2.style.borderRadius=30 + "px";
+    lazor2.style.zIndex = "5";
+    lazor2.style.boxShadow ="inset 0 10px 1px white, 0 0 7px red";
+    lazor2.style.backgroundColor = "red";
+    lazor2.style.TransformOrigin = "50% 100%";
+    lazor2.style.transform = "rotate(" + rad1 + "rad)";
+
+    lazor.style.left =offsets1.left+5 + "px";
+    lazor.style.top  =offsets1.top+6 + "px";
+
+
+
+
+    lazor2.style.left =offsets2.left+5 + "px";
+    lazor2.style.top  =offsets2.top+6 + "px";
+    lazor2.style.transform = "rotate(" + rad2 + "rad)";
+
+        las=true;
+
+
+
+
+
+}
+
+function checkLazerCollision(){
+    var ls =  document.getElementsByClassName('lazers');
+
+
+    for(var i=0; i<ls.length; i++) {
+        var cord =ls[i].getBoundingClientRect();
+
+        if(cord.left>playGround.clientWidth+playGround.offsetLeft||
+            cord.left<playGround.offsetLeft||
+            cord.top>playGroundHeight+playGround.offsetTop||
+            cord.top<playGround.offsetTop){
+            ls[i].parentNode.removeChild(ls[i]);
+        }
+
+
+    }
+
+}
+
+
+var tick = function() {
+
+
+
+
+    if (keyArrowLeft) { playerR-=10;}
+else if (keyArrowRight) {  playerR+=10;}
+
+     if(keyArrowUp){
+       playerX +=Math.cos(playerR*(Math.PI/180))*5;
+         playerY +=Math.sin(playerR*(Math.PI/180))*5;
+
+    }else if(keyArrowDown){
+         playerX -=Math.cos(playerR*(Math.PI/180))*5;
+         playerY -=Math.sin(playerR*(Math.PI/180))*5;
+    }
+
+
+
+
+
+
+
+
+if(las==true){
+    var ls =     document.getElementsByClassName('lazers');
+    var init = ls[0].style.transform.indexOf('(');
+    var fin =  ls[0].style.transform.indexOf(')');
+       for(var a=0;a<ls.length;a++) {
+           var len1 = parseInt(ls[a].style.width);
+           var top1= parseInt(ls[a].style.top);
+           var left1= parseInt(ls[a].style.left);
+           var rad1= parseFloat(ls[a].style.transform.substr(init+1,fin-init-1));
+
+
+           top1+= -Math.sin(rad1)*15;
+           left1+= -Math.cos(rad1)*15;
+
+
+           if(len1<=30){
+               len1+=5;
+           }
+
+           ls[a].style.left = left1.toString() + "px";
+           ls[a].style.top = top1.toString() + "px";
+           ls[a].style.width = len1.toString() + "px";
+
+       }
+
+   }
+    document.getElementById('player').style.left=playerX.toString() + "px";
+    document.getElementById('player').style.top=playerY.toString() + "px";
+    document.getElementById('player').style.transform = "rotate(" + playerR + "deg)";
+
+
+
+};
+
+
+function getPlayerPosition() {
+    var player = document.getElementById('player');
+    var playerWidth = player.clientWidth;
+    var playerHeight = player.clientHeight;
+    var playerCornerX = parseInt(player.style.left.substr(0, player.style.left.length - 2));
+    var playerCornerY = parseInt(player.style.top.substr(0, player.style.top.length - 2));
+    //console.log(player);
+    var playerX = playerCornerX + playerWidth / 2;
+    var playerY = playerCornerY + playerHeight / 2;
+    var playerPos = [];
+    playerPos[0] = playerX;
+    playerPos[1] = playerY;
+    return playerPos;
+    //console.log('playerX:' + playerX + ' playerY:' + playerY);
+}
+
+function moveEnemies(movement) {
+    var allEnemies = document.getElementsByClassName('enemy');
+    var userPosReal = getPlayerPosition();
+    for (var i in allEnemies) {
+        if (allEnemies[i].nodeName === 'DIV') {
+            var enemyRadius = parseInt(allEnemies[i].style.width.toString().substr(0, allEnemies[i].style.width.toString().length - 2)) / 2;
+            if (allEnemies[i].style.left == '') {
+                allEnemies[i].style.left = parseInt(allEnemies[i].offsetLeft) + 'px';
+                allEnemies[i].style.top = parseInt(allEnemies[i].offsetTop) + 'px';
+                userPos[0] -= enemyRadius;
+                userPos[1] -= enemyRadius;
+            }
+            var enemyX = parseFloat(allEnemies[i].style.left.toString().substr(0, allEnemies[i].style.left.toString().length - 2));
+            var enemyY = parseFloat(allEnemies[i].style.top.toString().substr(0, allEnemies[i].style.top.toString().length - 2));
+            var enemyCenterX = enemyX - enemyRadius;
+            var enemyCenterY = enemyY - enemyRadius;
+            var userPos = [userPosReal[0] - enemyRadius, userPosReal[1] - enemyRadius];
+            var deltaX = userPos[0] - enemyX;
+            var deltaY = userPos[1] - enemyY;
+            var angleInDegrees = Math.atan(deltaY / deltaX);// * 180 / Math.PI;
+            if (Math.abs(userPos[0] - (enemyX + Math.cos(angleInDegrees) * movement)) < Math.abs(userPos[0] - (enemyX - Math.cos(angleInDegrees) * movement))) {
+                var newEnemyPosX = (enemyX + Math.cos(angleInDegrees) * movement);
+            } else {
+                var newEnemyPosX = (enemyX - Math.cos(angleInDegrees) * movement);
+            }
+            if (Math.abs(userPos[1] - (enemyY + Math.sin(angleInDegrees) * movement)) < Math.abs(userPos[1] - (enemyY - Math.sin(angleInDegrees) * movement))) {
+                var newEnemyPosY = (enemyY + Math.sin(angleInDegrees) * movement);
+            } else {
+                var newEnemyPosY = (enemyY - Math.sin(angleInDegrees) * movement);
+            }
+            allEnemies[i].style.left = newEnemyPosX + 'px';
+            allEnemies[i].style.top = newEnemyPosY + 'px';
+            //check collision
+            var distanceBetweenPlayer = Math.sqrt(Math.pow(newEnemyPosX - userPos[0], 2) + Math.pow(newEnemyPosY - userPos[1], 2));
+            if (distanceBetweenPlayer < playerHeight / 2 + enemyRadius) {
+                lives--;
+                //allEnemies[i].removeEventListener;
+                document.body.removeChild(allEnemies[i]);
+                document.getElementById('lives').innerText = parseInt(document.getElementById('lives').innerText) - 1;
+                document.getElementById('enemiesCount').innerText = parseInt(document.getElementById('enemiesCount').innerText) - 1;
+            }
+        }
+    }
+}
+
+
+
+window.onload =function movement(){
+    document.addEventListener('keydown', keyDown, false);
+    document.addEventListener('keyup', keyUp, false);
+    document.addEventListener('mousedown', createLasers, false);
+    function loop() {
+        tick();
+        setTimeout(loop, tickRate);
+        moveEnemies(2);
+    }
+
+    loop();
+};
---
+++ version3/rocks.js
@@ -0,0 +1,69 @@
+﻿
+var playGround = document.getElementById('gameholder');
+var playGroundWidth = playGround.clientWidth;
+var playGroundHeight = playGround.clientHeight;
+var playGroundCornerX = playGround.offsetLeft;
+var playGroundCornerY = playGround.offsetTop;
+
+
+setInterval(addEnemy, 250);
+
+function addEnemy() {
+    //enemy image Size:
+    var enemyWidht = 50;
+    var enemyHeight = 50;
+
+    var randomQuadrant = Math.floor(Math.random() * 8) + 0;
+    switch (randomQuadrant) {
+        case 0: {
+            randomX = Math.floor(Math.random() * (playGroundCornerX + 1)) - enemyWidht;
+            randomY = Math.floor(Math.random() * (playGroundCornerY + 1)) - enemyHeight;
+            break;
+        }
+        case 1: {
+            randomX = Math.floor(Math.random() * (playGroundWidth + 1)) + playGroundCornerX - enemyWidht;
+            randomY = Math.floor(Math.random() * (playGroundCornerY + 1)) - enemyHeight;
+            break;
+        }
+        case 2: {
+            randomX = Math.floor(Math.random() * (playGroundCornerX + 1)) + playGroundWidth + playGroundCornerX - enemyWidht;
+            randomY = Math.floor(Math.random() * (playGroundCornerY + 1)) - enemyHeight;
+            break;
+        }
+        case 3: {
+            randomX = Math.floor(Math.random() * (playGroundCornerX + 1)) - enemyWidht;;
+            randomY = Math.floor(Math.random() * (playGroundHeight + 1)) + playGroundCornerY - enemyHeight;;
+            break;
+        }
+        case 4: {
+            randomX = Math.floor(Math.random() * (playGroundCornerX + 1)) + playGroundWidth + playGroundCornerX;
+            randomY = Math.floor(Math.random() * (playGroundHeight + 1)) + playGroundCornerY - enemyHeight;
+            break;
+        }
+        case 5: {
+            randomX = Math.floor(Math.random() * (playGroundCornerX + 1)) - enemyWidht;
+            randomY = Math.floor(Math.random() * (playGroundCornerY + 1 + enemyHeight)) + playGroundCornerY + playGroundHeight - enemyHeight;
+            break;
+        }
+        case 6: {
+            randomX = Math.floor(Math.random() * (playGroundWidth + 1 + enemyWidht)) + playGroundCornerX - enemyWidht;
+            randomY = Math.floor(Math.random() * (playGroundCornerY + 1)) + playGroundCornerY + playGroundHeight;
+            break;
+        }
+        case 7: {
+            randomX = Math.floor(Math.random() * (playGroundCornerX + 1)) + playGroundWidth + playGroundCornerX;
+            randomY = Math.floor(Math.random() * (playGroundCornerY + 1 + enemyHeight)) + playGroundCornerY + playGroundHeight - enemyHeight;
+            break;
+        }
+        default: { randomX = 0; randomY = 0; }
+    }
+    var newEnemy = document.createElement('div');
+    newEnemy.style.width = enemyWidht + 'px';
+    newEnemy.style.height = enemyHeight + 'px';
+    newEnemy.style.left = (randomX) + 'px';
+    newEnemy.style.top = (randomY) + 'px';
+    newEnemy.setAttribute('class', 'enemy');
+    newEnemy.style.borderRadius = enemyWidht / 2 + 'px';
+    document.body.appendChild(newEnemy);
+    document.getElementById('enemiesCount').innerText = parseInt(document.getElementById('enemiesCount').innerText) + 1;
+}
